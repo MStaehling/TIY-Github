bfc6898061e0b13f621adf9901cf649a52f0f680
Michael Staehling <Michael.staehling@gmail.com>
2015-06-09 23:11:07 -0400

    replaced alot of placeholders but still won't connect

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+(function(window) {
+    $.getJSON("../../api/github/users/octocat.json")
+        .then(function(octocat) {
+            console.log(octocat);
+
+            $name = $('#name');
+            $name.text(octocat.name);
+
+            $avatar = $('img', '#avatar');
+            $avatar.attr('src', octocat.avatar_url);
+
+            $login = $('#login');
+            $login.text(octocat.login);
+
+            $location = $('#location');
+            $location.text(octocat.location);
+
+            $github = $('#github');
+            $github.text(octocat.company);
+
+            $email = $('a', '#e-mail');
+            $email.attr('href', 'mailto:' + octocat.email);
+
+            $blog = $('a', '#blog');
+            $blog.attr('href', octocat.blog);
+
+            $followers = $('#followers-count');
+            $followers.text(octocat.followers);
+
+            $following = $('#following-count');
+            $following.text(octocat.following);
+
+            $starred = $('#star-count');
+            $starred.text(octocat.starred_url.length);
+            // just set it to
+            // $('star-count') = octocat.starred_url.length;
+
+
+
+        });
+
+
+})(window);
