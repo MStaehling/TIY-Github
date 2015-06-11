d46cc7ca122d50886756ffa276b5a9fd83881ced
Michael Staehling <Michael.staehling@gmail.com>
2015-06-11 12:30:06 -0400

    class work kinda follow

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,17 @@
     $.getJSON("../../api/github/users/mstaehling.json")
         .then(function(mstaehling) {
             console.log(mstaehling);
+            /**
+             * Replacing placeholders with data from....?
+             * @param
+             * @return
+             */
+             var nameTpl = _.template($('#name').html());
+             
 
-            $name = $('#name');
-            $name.text(mstaehling.name);
+
+            // $name = $('#name');
+            // $name.text(mstaehling.name);
 
             $avatar = $('img#avatar');
             $avatar.attr('src', mstaehling.avatar_url);
@@ -18,7 +26,7 @@
             // $github = $('#github');
             // $github.text(octocat.company);
 
-// refactor
+            // refactor
             // $email = $('a#e-mail');
             // $email.attr('href', 'mailto:' + octocat.email);
             //
@@ -32,7 +40,7 @@
             $following.text(mstaehling.following);
 
 
-// refactor
+            // refactor
             // $starred = $('#star-count');
             // $starred[octocat.starred_url].length;
             // just set it to
@@ -41,6 +49,53 @@
 
 
         });
-_.pluck("../../api/github/users/MStaehling/mstaehling-repos.json", mstaehling.repos_url);
-// _.every("../../api/github/users/MStaehling/mstaehling-repos.json", mstaehling.repos_url);
+    $(function() {
+        $('.tabs')
+            /**
+             * Event Listenener
+             * - Listening for: click on 'a[href^=='#']'
+             * - What element: .tabs (parent of the parent of the '<a>')
+             * - What happens:event.preventDefault prevents the default behavior of an element
+             */
+            .on('click', 'a[href^='#
+                ']',
+                function(event) {
+                    event.preventDefault();
+                    $(this).add(this.hash).trigger('activate');
+
+                    // return  false; // This is bad....does event.preventDefault() and event.stopPropagation()
+                })
+
+        /**
+         * Event Listener:
+         * - What event: activate on '.tabs li'
+         * - What element: '.tabs'
+         * - What happens:
+         */
+        .on('activate', 'li', activator)
+
+        /**
+         * activate the first '<li>' in '.tabs'
+         */
+        .children('li:first')
+            .trigger('activate');
+
+        function activator(event) {
+            $(event.target) // '.tabs li' or 'event.target'
+                .addClass('active');
+            .siblings()
+                .removeClass('active');
+        }
+        $('.panel')
+            /**
+             * Event Listener
+             * - What event: 'activate' on '.panel'
+             * - What element: '.panel'
+             * - What happens:
+             */
+
+    })
+
+    // _.pluck("../../api/github/users/MStaehling/mstaehling-repos.json", mstaehling.repos_url);
+    // _.every("../../api/github/users/MStaehling/mstaehling-repos.json", mstaehling.repos_url);
 })(window);
